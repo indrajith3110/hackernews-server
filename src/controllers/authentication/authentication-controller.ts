9e6006bb27f9bd234a55ae26ef73274902f3d2ce
indrajith3110 <indrajithsomaiah2310@gmail.com>
2025-04-04 16:23:14 +0530

    fix: update jwt import syntax and add missing newlines in authentication controller and token middleware

--- src/controllers/authentication/authentication-controller.ts
+++ src/controllers/authentication/authentication-controller.ts
@@ -8,6 +8,7 @@ import {
 import { prismaClient } from "../../extras/prisma";
 import * as jwt from "jsonwebtoken";
 import { jwtSecretKey } from "../../environment";
+
 export const signUpWithUsernameAndPassword = async (parameters: {
   username: string;
   password: string;
@@ -17,13 +18,16 @@ export const signUpWithUsernameAndPassword = async (parameters: {
   const isUserExistingAlready = await checkIfUserExistsAlready({
     username: parameters.username,
   });
+
   if (isUserExistingAlready) {
     throw SignUpWithUsernameAndPasswordError.CONFLICTING_USERNAME;
   }
+
   // Create password hash
   const passwordHash = await createPasswordHash({
     password: parameters.password,
   });
+
   // Create new user
   const user = await prismaClient.user.create({
     data: {
@@ -32,16 +36,19 @@ export const signUpWithUsernameAndPassword = async (parameters: {
       name: parameters.name,
     },
   });
+
   // Generate JWT token
   const token = createJWToken({
     id: user.id,
     username: user.username,
   });
+
   return {
     token,
     user,
   };
 };
+
 export const logInWithUsernameAndPassword = async (parameters: {
   username: string;
   password: string;
@@ -50,6 +57,7 @@ export const logInWithUsernameAndPassword = async (parameters: {
   const passwordHash = createPasswordHash({
     password: parameters.password,
   });
+
   // Find user
   const user = await prismaClient.user.findUnique({
     where: {
@@ -57,37 +65,45 @@ export const logInWithUsernameAndPassword = async (parameters: {
       password: passwordHash,
     },
   });
+
   if (!user) {
     throw LogInWtihUsernameAndPasswordError.INCORRECT_USERNAME_OR_PASSWORD;
   }
+
   // Generate JWT token
   const token = createJWToken({
     id: user.id,
     username: user.username,
   });
+
   return {
     token,
     user,
   };
 };
+
 const createJWToken = (parameters: { id: string; username: string }): string => {
   const jwtPayload: jwt.JwtPayload = {
     iss: "hackernews-server",
     sub: parameters.id,
     username: parameters.username,
   };
+
   return jwt.sign(jwtPayload, jwtSecretKey, {
     expiresIn: "30d",
   });
 };
+
 const checkIfUserExistsAlready = async (parameters: { username: string }): Promise<boolean> => {
   const existingUser = await prismaClient.user.findUnique({
     where: {
       username: parameters.username,
     },
   });
+
   return !!existingUser;
 };
+
 const createPasswordHash = (parameters: { password: string }): string => {
   return createHash("sha256").update(parameters.password).digest("hex");
 };
